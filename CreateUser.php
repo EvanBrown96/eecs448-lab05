e2b22dda03cec602860083f87b8f91edaa73d95f
EvanBrown96 <evan.br96@yahoo.com>
2018-10-17 23:18:31 -0500

    added backend file to createuser

---
+++ CreateUser.php
@@ -0,0 +1,48 @@
+<?php
+
+// get entered username from POST data
+$username = $_POST["username"];
+
+// check if a blank username was entered
+if($username == ""){
+  echo("You must enter a non-blank username!");
+  exit();
+}
+
+// create mysql connection
+$mysqli = new mysqli("mysql.eecs.ku.edu", "e329b498", "pu4oefeL", "e329b498");
+
+// check that the connection was successful
+if($mysqli->connect_errno){
+  echo("Connection to MySQL server failed: " . $mysqli->connect_errno);
+  exit();
+}
+
+// see if this user has already been entered
+$query = "SELECT * FROM users WHERE user_id='" . $username . "';";
+
+if($result = $mysqli->query($query)){
+
+  while($row = $result->fetch_assoc()){
+    if($row["user_id"] == $username){
+      echo("The username " . $username . " is already in use, try again.");
+      exit();
+    }
+  }
+  $result->free();
+}
+
+// add this user to the table
+$query = "INSERT INTO users VALUES ('" . $username . "')";
+
+if($mysqli->query($query) === TRUE){
+  echo("Username " . $username . " successfully registered in database!");
+}
+else{
+  echo("Error pusing username to database: " . $mysqli->error);
+}
+
+// close connection
+$mysqli->close();
+
+?>
